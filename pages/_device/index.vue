c6ca6031da81846af7f89b83d8fe26e0d53af1f2
PurrplingCat <ellenfawkes@gmail.com>
2018-01-01 20:26:08 +0100

    page

---
+++ pages/_device/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Hello, {{ deviceName }}</h2>
+</template>
+
+<<script>
+export default {
+  asyncData({ params }) {
+    return {
+      deviceName: params.device
+    }
+  }
+}
+</script>
