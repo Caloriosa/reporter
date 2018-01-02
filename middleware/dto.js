f11d084958442d652b07800f0360b5636be3186d
Ellen Fawkes <fawkes@ttc.cz>
2018-01-02 11:15:27 +0100

    Removed process.server

--- middleware/dto.js
+++ middleware/dto.js
@@ -2,7 +2,5 @@ import { createApiClient } from '@caloriosa/rest-dto'
 
 export default function ({ app, req }) {
   let api = createApiClient();
-  if (process.server) {
-    app.$api = api;
-  }
+  app.$api = api;
 }
