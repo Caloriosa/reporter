bea34f94a7b6c2882ecc517f924b93bdebf1ac0c
PurrplingCat <ellenfawkes@gmail.com>
2018-01-03 21:08:55 +0100

    navigation style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" fixed clipped light overflow app>
+    <v-navigation-drawer v-model="drawer" temporary fixed clipped light overflow app>
       <v-list>
-        <v-list-tile v-for="item in items" :key="item.title" :to="item.href">
+        <v-list-tile v-for="item in items" :key="item.title" :to="item.href" exact>
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -13,10 +13,11 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar dark clipped-left fixed app>
-      <v-btn icon @click.stop="drawer = !drawer">
-        <v-icon medium>mdi-thermometer-lines</v-icon>
-      </v-btn>
-      <v-toolbar-title class="mr-4 ml-4">{{title}}</v-toolbar-title>
+      <v-toolbar-side-icon class="hidden-md-and-up" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="mr-4 ml-4">
+        <v-icon>mdi-thermometer-lines</v-icon>
+        {{title}}
+      </v-toolbar-title>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn flat to="/" exact>World</v-btn>
         <v-btn flat to="/cloud" exact>Cloud</v-btn>
@@ -43,11 +44,11 @@
   export default {
     data: () => ({
       drawer: false,
-      title: 'Caloriosa Reporter',
+      title: 'Caloriosa World',
       items: [
-        { title: 'Home', icon: 'dashboard', href: '//caloriosa.com' },
-        { title: 'World', icon: 'world', href: '/' },
-        { title: 'Cloud', icon: 'cloud', href: '/cloud' }
+        { title: 'World map', icon: 'mdi-earth', href: '/' },
+        { title: 'Tag cloud', icon: 'cloud', href: '/cloud' },
+        { title: 'Popular', icon: 'star', href: '/popular' }
       ]
     }),
     props: {
