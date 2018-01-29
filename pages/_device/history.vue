04baae7609a0ec8803a59850dcb64c0ab713b19b
Ellen Fawkes <fawkes@ttc.cz>
2018-01-29 14:04:48 +0100

    History date picker

--- pages/_device/history.vue
+++ pages/_device/history.vue
@@ -1,3 +1,20 @@
 <template>
-
+  <v-layout row wrap>
+    <v-flex column md12 lg4>
+      <v-date-picker v-model="picker"></v-date-picker>
+    </v-flex>
+    <v-flex column md12 lg8>
+      {{ picker }}
+    </v-flex>
+  </v-layout>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      picker: null
+    }
+  }
+}
+</script>
