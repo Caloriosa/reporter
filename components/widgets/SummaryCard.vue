b383f49ed42e6bbfb97ce6b7a917cf7688de887c
Ellen Fawkes <fawkes@ttc.cz>
2018-01-29 13:43:37 +0100

    cards as component

---
+++ components/widgets/SummaryCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card :color="color" class="white--text">
+    <v-container fluid grid-list-lg>
+      <v-layout row>
+        <v-flex xs9>
+          <div>
+            <div class="caption">{{measure.type}}</div>
+            <div class="mt-2 mb-2 display-1">{{ tValue(measure) }}</div>
+            <div class="caption">{{ measure.title }}</div>
+          </div>
+        </v-flex>
+        <v-flex xs3 hidden-md-and-down text-xs-center pt-4>
+          <v-icon color="white" x-large>{{tIcon(measure.type)}}</v-icon>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'primary'
+    },
+    unit: {
+      type: String
+    },
+    measure: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    tIcon (type) {
+      switch (type) {
+        case 'TEMPERATURE':
+          return 'mdi-thermometer'
+        case 'HUMIDITY':
+          return 'mdi-water-percent'
+        case 'WIND_SPEED':
+          return 'mdi-weather-windy'
+        default:
+          return 'mdi-help'
+      }
+    },
+    tValue (measure) {
+      switch (measure.type) {
+        case 'TEMPERATURE':
+          return `${measure.value} ${this.unit || '°C'}`
+        case 'HUMIDITY':
+          return (measure.value * 100) + ' %'
+        case 'WIND_SPEED':
+          return `${measure.value} ${this.unit || 'm/s'}`
+        default:
+          return measure.value
+      }
+    }
+  }
+}
+</script>
