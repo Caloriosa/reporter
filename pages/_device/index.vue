38190e683f5754650c0052db3fcddd0f15b28e09
Ellen Fawkes <fawkes@ttc.cz>
2018-01-29 12:16:11 +0100

    Navigation in device

--- pages/_device/index.vue
+++ pages/_device/index.vue
@@ -1,26 +1,6 @@
 <template>
   <v-container grid-list-md>
     <v-layout d-block>
-      <v-layout v-if="current" row wrap>
-        <v-flex lg2 v-for="(m, i) in current" :key="i">
-          <v-card :color="`${colors[i]} darken-2`" class="white--text">
-            <v-container fluid grid-list-lg>
-              <v-layout row>
-                <v-flex xs9>
-                  <div>
-                    <div class="caption">{{m.type}}</div>
-                    <div class="mt-2 mb-2 display-1">{{ m.value }}°C</div>
-                    <div class="caption">{{ m.title }}</div>
-                  </div>
-                </v-flex>
-                <v-flex xs3 hidden-md-and-down text-xs-center pt-4>
-                  <v-icon color="white" x-large>{{tIcon(m.type)}}</v-icon>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card>
-        </v-flex>
-      </v-layout>
       <v-layout row wrap>
         <v-flex column md6>
           <v-card>
@@ -85,33 +65,6 @@ export default {
   },
   asyncData ({ params }) {
     return {
-      current: [
-        {
-          value: 25.1,
-          title: 'Outside',
-          type: 'TEMPERATURE'
-        },
-        {
-          value: 23.5,
-          title: 'Inside',
-          type: 'TEMPERATURE'
-        },
-        {
-          value: 0.65,
-          title: 'Outside humidity',
-          type: 'HUMIDITY'
-        },
-        {
-          value: 0.52,
-          title: 'Inside humidity',
-          type: 'HUMIDITY'
-        },
-        {
-          value: 6.7,
-          title: 'Wind speed',
-          type: 'WIND_SPEED'
-        }
-      ],
       summary: {
         items: [
           {
@@ -127,21 +80,6 @@ export default {
             average: 20.1
           }
         ]
-      },
-      colors: [ 'primary', 'secondary', 'green', 'red', 'cyan', 'purrple' ]
-    }
-  },
-  methods: {
-    tIcon (type) {
-      switch (type) {
-        case 'TEMPERATURE':
-          return 'mdi-thermometer'
-        case 'HUMIDITY':
-          return 'mdi-water-percent'
-        case 'WIND_SPEED':
-          return 'mdi-weather-windy'
-        default:
-          return 'mdi-help'
       }
     }
   }
