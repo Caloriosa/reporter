64f636bd2fbda119b92ed5f4f798cc5f96a8955c
PurrplingCat <ellenfawkes@gmail.com>
2018-01-31 21:27:36 +0100

    Error box design

--- components/widgets/MapErrorBox.vue
+++ components/widgets/MapErrorBox.vue
@@ -7,18 +7,20 @@
       </v-btn>
     </v-card-actions>
     <v-card-title>
-    <div>
-      <v-icon large color="red darken-3">error</v-icon>
-    </div>
-    <div class="px-4">
+    <v-flex column hidden-xs-only sm1 class="text-xs-center">
+      <v-icon x-large color="red darken-3">error</v-icon>
+    </v-flex>
+    <v-flex column xs12 sm11 class="px-3">
       <div class="headline">{{ error.message }}</div>
-      <span class="grey--text">Please check our query or contact support.</span>
-    </div>
+      <span class="grey--text">
+        Please check our query, check service status or <nuxt-link to="#">contact support.</nuxt-link>
+      </span>
+    </v-flex>
     </v-card-title>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn flat color="red darken-3" to="#" nuxt>Contact support</v-btn>
-      <v-btn v-if="error.query" flat color="red darken-3" :to="`search/${error.query}`" nuxt>Search in fulltext</v-btn>
+      <v-btn flat color="red darken-3" to="#" nuxt>Check Status</v-btn>
+      <v-btn v-if="error.query" flat color="red darken-3" :to="`search/${error.query}`" nuxt>Search all</v-btn>
     </v-card-actions>
   </v-card>
 </template>
