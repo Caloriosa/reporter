35f724620bee35279979ad89f22dcef1186ec9e0
Ellen Fawkes <fawkes@ttc.cz>
2018-02-05 13:57:50 +0100

    Redesigned error screen

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,31 +1,31 @@
 <template>
-  <v-app>
-    <v-layout justify-start>
-      <v-layout class="error-msg py-4" :class="`${customError.color} lighten-1`" column justify-center align-center>
+  <v-layout row wrap>
+    <v-flex xs12 justify-start class="error-msg" :class="`${customError.color} lighten-1`">
+      <v-layout column justify-center align-center class="error-container py-5">
         <h3 class="ma-2 display-2" v-text="customError.emoticon"></h3>
         <h1 class="ma-2 display-4" v-text="customError.code"></h1>
         <h4 class="ma-3 headline" v-text="customError.message"></h4>
-        <div class="ma-4">
+        <div class="my-4">
           <v-btn outline large color="white" :to="{name: 'index'}" nuxt>Go Home</v-btn>
           <v-btn outline large :color="`${customError.color} darken-4`" to="#" nuxt>Check status</v-btn>
         </div>
       </v-layout>
-    </v-layout>
-    <v-layout row justify-center class="error-footer">
-      <div>&copy; {{ new Date().getFullYear() }} Caloriosa</div>
-      <ul class="footer-menu big-spaces">
+    </v-flex>
+    <v-flex xs12 class="error-footer">
+      <v-layout justify-center >
+      <ul class="error-menu">
         <li><a href="#">Contact Support</a></li>
         <li><a href="#">Main Page</a></li>
         <li><a href="#">Github Issues</a></li>
       </ul>
-    </v-layout>
-  </v-app>
+      </v-layout>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
   props: ['error'],
-  layout: 'empty',
   computed: {
     customError () {
       let code = this.error.statusCode || this.error.status || 500
@@ -47,6 +47,15 @@ export default {
 
 <style lang="stylus" scoped>
   .error-footer {
-    margin: 6em 0;
+    margin: 6vh 0;
+  }
+  .error-container {
+    min-height: 50vh;
+  }
+  ul.error-menu {
+    li {
+      display: inline-block;
+      margin: 1.5vw;
+    }
   }
 </style>
