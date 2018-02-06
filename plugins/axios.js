eca5774e4371e9c277a73dc640bb8d5d73d01c90
Ellen Fawkes <fawkes@ttc.cz>
2018-02-06 16:38:28 +0100

    Removed security hole (forgotten dev debug)

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,5 +2,4 @@ export default function ({ $axios, redirect }) {
   $axios.setHeader('X-Dto-Client', process.env.API_CLIENT_ID)
   $axios.setHeader('X-Application', process.env.API_APP_SIGNATURE || null);
   $axios.setHeader('X-Agent-Type', 'user')
-  console.log(process.env.API_APP_SIGNATURE)
 }
