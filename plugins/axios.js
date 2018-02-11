e9de60c89a7690c3f140f1586b7d2a28c5efd9f5
PurrplingCat <ellenfawkes@gmail.com>
2018-02-11 10:05:48 +0100

    Axios server side logging

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,4 +2,18 @@ export default function ({ $axios, redirect }) {
   $axios.setHeader('X-Dto-Client', process.env.API_CLIENT_ID)
   $axios.setHeader('X-Application', process.env.API_APP_SIGNATURE || null)
   $axios.setHeader('X-Agent-Type', 'user')
+
+  $axios.onResponse(res => {
+    if (process.server) console.dir(res.data)
+  })
+  $axios.onError(err => {
+    if (!process.server) return
+    if (err.response && err.response.data && err.response.data.error) {
+      let dError = err.response.data.error
+      console.dir(err.response.data)
+      console.error(`[axios] Error: ${dError.status} ${dError.type} - ${dError.message}`)
+    }
+    console.error(`[axios] ${err.stack}`)
+  }
+  )
 }
